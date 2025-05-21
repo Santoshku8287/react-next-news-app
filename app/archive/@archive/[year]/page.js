a8b57f1208972b62cd079670f64f626d56ab8202
Santosh Kumar <santoshku8287@gmail.com>
2025-05-21 22:36:55 +0530

    archive page handled with year and month and default latest page

--- app/archive/@archive/[year]/page.js
+++
@@ -1,8 +0,0 @@
-import { getNewsForYear } from "@/lib/news";
-import NewsList from "@/lib/news-list";
-
-export default function FilteredNewsPage({params}){
-    const newsYear = params.year;
-    const news = getNewsForYear(newsYear)
-    return <NewsList news={news} />
-}
