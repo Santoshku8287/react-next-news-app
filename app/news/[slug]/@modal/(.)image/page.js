df49f3d87a21a9857390d3a8687f52c4fe7b8423
Santosh Kumar <santoshku8287@gmail.com>
2025-05-23 21:32:08 +0530

    full screenimage issue fix

--- app/news/[slug]/@modal/(.)image/page.js
+++ app/news/[slug]/@modal/(.)image/page.js
@@ -10,13 +10,12 @@ export default function InterceptedImagePage({ params }) {
   }
   return (
     <>
-      <div className="modal-backdrop">
+      <div className="modal-backdrop"/>
         <dialog className="modal" open>
           <div className="fullscreen-image">
             <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
           </div>
         </dialog>
-      </div>
     </>
   );
 }
